a6d4bfd4af98f24919ec1a2fcc49ec5754f6b022
Aman Meena <amanmeena@yahoo.com>
2020-04-29 12:31:31 +0530

    Completed UI.

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -15,6 +15,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // MARK:- IBOutlets
+    
+    // Header
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var headerView: UIView!
+    
+    // Tip Input
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: UITextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    // Tip Output
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    // Reset Button
+    @IBOutlet weak var resetButton: UIButton!
+    
+    // MARK:- IBActions
 
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch toggled on")
+        } else {
+            print("switch toggled off")
+        }
+    }
+    
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
+        print(sender.selectedSegmentIndex)
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        print("Reset button tapped!")
+    }
+    
+    
 }
 
